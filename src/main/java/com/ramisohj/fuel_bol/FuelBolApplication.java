8ad760e8ccb55b451da1bb05b5e9575830f0943a
jhosimar <ramisohj@gmail.com>
2025-03-22 18:09:55 -0400

    added initial spring-boot configuration

---
+++ src/main/java/com/ramisohj/fuel_bol/FuelBolApplication.java
@@ -0,0 +1,13 @@
+package com.ramisohj.fuel_bol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelBolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelBolApplication.class, args);
+	}
+
+}
