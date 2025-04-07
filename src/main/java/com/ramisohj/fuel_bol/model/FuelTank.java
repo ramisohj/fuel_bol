c28109391b29b7baac860342143d93d1f9e41972
jhosimar <ramisohj@gmail.com>
2025-04-07 16:38:24 -0400

    removed batch operation using JPA

--- src/main/java/com/ramisohj/fuel_bol/model/FuelTank.java
+++ src/main/java/com/ramisohj/fuel_bol/model/FuelTank.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,15 +21,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class FuelTank {
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "fuel_tank_seq"
-    )
-    @SequenceGenerator(
-            name = "fuel_tank_seq",
-            sequenceName = "fuel_tank_seq",
-            allocationSize = 100
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idFuelTank;
     private Long idMonitoring;
     private Long idFuelStation;
